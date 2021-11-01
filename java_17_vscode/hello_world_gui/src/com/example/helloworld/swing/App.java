dc37ea6d5db11a21381d35e711a45aea3ffab78e
Paul Watts <7057417+PaulFWatts@users.noreply.github.com>
2021-11-02 04:08:23 +1100

    Added Java

---
+++ java_17_vscode/hello_world_gui/src/com/example/helloworld/swing/App.java
@@ -0,0 +1,10 @@
+package com.example.helloworld.swing;
+import javax.swing.*;
+
+public class App {
+
+public static void main(String args[]) {
+    JOptionPane.showMessageDialog( null, "Hello World!" );
+}
+
+}
